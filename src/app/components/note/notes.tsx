d3b4b0704023b15b1ffc74b5b010d6a15b8ed497
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-03-27 23:53:12 +0100

    working on the * path

--- src/app/components/note/notes.tsx
+++ src/app/components/note/notes.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Notes = () => {
-  return <div>notes</div>;
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente itaque
+      asperiores amet, modi dolore ullam harum voluptatem autem laborum quaerat!
+    </div>
+  );
 };
 
 export default Notes;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Notes from "../app/components/note/notes";
-import { Navigate } from "react-router-dom";
-
 export default function Home() {
   return (
     <main>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Notes />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </main>
